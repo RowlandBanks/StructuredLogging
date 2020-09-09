16807968bcb9d0a883ec661a3ab4f518d6ec2e83
RowlandBanks <rowland.banks@gmail.com>
2020-09-09 22:24:57 +0100

    Added sample application.

---
+++ src/SampleApp/LoggingService.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+
+namespace Arbee.StructuredLogging.SampleApp
+{
+    internal class LoggingService : BackgroundService
+    {
+        private readonly ILogger<LoggingService> _logger;
+
+        public LoggingService(ILogger<LoggingService> logger)
+        {
+            _logger = logger ?? throw new ArgumentNullException(nameof(logger));
+        }
+
+        protected override Task ExecuteAsync(CancellationToken stoppingToken)
+        {
+            return Task.Run(() =>
+            {
+                while (!stoppingToken.IsCancellationRequested)
+                {
+                    _logger.Log(LogLevel.Information, new
+                    {
+                        Message = "User Registered"
+                    });
+
+                    Task.Delay(TimeSpan.FromSeconds(5)).Wait();
+                }
+            });
+        }
+    }
+}
---
+++ src/SampleApp/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using Arbee.StructuredLogging.MicrosoftExtensions.Extensions;
+using Arbee.StructuredLogging.SampleApp;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+
+namespace SampleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var host = Host.CreateDefaultBuilder()
+                .ConfigureServices(services =>
+                {
+                    services
+                        .AddHostedService<LoggingService>();
+                })
+                .ConfigureLogging(builder =>
+                {
+                    builder
+                        .AddConsole()
+                        .AddStructuredLogging();
+                })
+                .Build();
+
+            host.Run();
+        }
+    }
+}
